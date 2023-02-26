951ce36e731e3cace131f4613a4a5a1edfc0dae8
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-02-26 03:35:51 +0100

    Insert and Select data to database

---
+++ 03-insert-users-data.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO users (yearly_salary, full_name, current_status)
+-- VALUES (19000, 'Hamza Zukorlic', 'self-employed');
+
+-- INSERT INTO users (yearly_salary, full_name, current_status)
+-- VALUES (24000, 'Kadir Zukorlic', 'employed');
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+ VALUES (0, 'Ne znam', 'unemployed');
