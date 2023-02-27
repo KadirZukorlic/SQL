af7de8bc59d46fa242ca676cdeb35f7065bff399
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-02-27 19:32:26 +0100

    Delete & update tables

---
+++ 10-insert-data-again.sql
@@ -0,0 +1,11 @@
+INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+ VALUES ('Learning', 'Billiger Strasse 08, Munich', 0.87, TRUE );
+
+INSERT INTO employers (company_name, company_address, yearly_revenue)
+ VALUES ('Big Oil Inc', 'slippery Strasse 08, Dusseldorf', 112.55);
+
+INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+ VALUES ('Fast Food', 'Etwas Strasse 08, Berlin', 6.12, TRUE );
+
+INSERT INTO conversations (user_name, employer_name, message)
+ VALUES ('Hamza Zukorlic', 'Big Oil Inc', 'Hi I like learning backend a lot');
