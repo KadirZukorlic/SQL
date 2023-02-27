05accb392f477c4e401dc79ffa00a09aaa12580e
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-02-27 19:16:25 +0100

    Insert data into conversations & employers

---
+++ 07-insert-data.sql
@@ -0,0 +1,11 @@
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+--  VALUES ('CompanyX', 'Billiger Strasse 08, Munich', 0.87, TRUE );
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+--  VALUES ('Big Oil Inc', 'slippery Strasse 08, Dusseldorf', 112.55, FALSE );
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+--  VALUES ('CompanyX', 'Etwas Strasse 08, Berlin', 6.12, TRUE );
+
+INSERT INTO conversations (user_name, employer_name, message, date_sent)
+ VALUES ('Hamza Zukorlic', 'Big Oil Inc', 'Hi I like learning backend a lot', '2023-01-15 16:32:10');
