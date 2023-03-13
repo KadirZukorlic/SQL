32ca8ff31affb986af013c48d0a6b715241dcb89
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-03-13 01:57:09 +0100

    Alias and manipulate value with select (divide, multiply etc)

--- CRUD/06-select-data.sql
+++ CRUD/06-select-data.sql
@@ -1,5 +1,6 @@
-SELECT date_created,
+SELECT 
+    date_created,
     customer_name, 
     product_name, 
-    volume 
+    volume / 1000 AS total_sales -- AS updates column name in query result set and / 1000 divides by 1000
 FROM sales;
