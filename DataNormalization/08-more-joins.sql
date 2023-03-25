1bc995fcc23ca1a3c42d5bd5775b3067de5ee9ab
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-03-25 14:37:36 +0100

    Add more Left joins and Cross join

---
+++ DataNormalization/08-more-joins.sql
@@ -0,0 +1,5 @@
+-- Select all cities and users data
+SELECT c.name AS city_name, u.first_name, u.last_name, a.street, a.house_number
+FROM cities AS c
+LEFT JOIN adresses AS a ON c.id = a.city_id
+LEFT JOIN users AS u ON u.address_id = a.id;
