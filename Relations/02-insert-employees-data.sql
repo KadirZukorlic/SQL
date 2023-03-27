556b32cef70535ec761303a83fc53a4cdb0cc0af
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-03-27 03:31:13 +0200

    Add employees and intranet_account data

---
+++ Relations/02-insert-employees-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (first_name, last_name, birthdate, email)
+VALUES('Hamza', 'Zukorlic', '1993-01-22', 'hamza@test.com');
+
+INSERT INTO intranet_accounts (email, password)
+VALUES('hamza@test.com', 'alwaysencryptpassword');
