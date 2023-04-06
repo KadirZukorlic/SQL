12e13f364b1b6895ff9add5fc6e05085009219b5
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-04-06 17:03:36 +0200

    Delete some data

---
+++ Relations/10-delete-data.sql
@@ -0,0 +1,8 @@
+-- DELETE FROM company_buildings
+-- WHERE id = 2; -- Sets building_id to NULL cause we defined ON DELETE SET NULL
+
+-- DELETE FROM teams
+-- WHERE id = 2;
+
+DELETE FROM employees
+WHERE id = 1;
