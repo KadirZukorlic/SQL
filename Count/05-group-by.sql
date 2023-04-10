6547e4703868208631b8e35537a42f4fcb32c8f6
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-04-10 16:37:53 +0200

    Group By

---
+++ Count/05-group-by.sql
@@ -0,0 +1,6 @@
+-- SELECT DISTINCT booking_date, SUM(num_guests)
+-- FROM bookings; -- Doesn't work because DISTINCT duplicate values only
+
+-- SELECT booking_date, SUM(num_guests)
+-- FROM bookings
+-- GROUP BY booking_date;
