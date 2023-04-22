5da8b017ae8759a5255b48e5ea4cc6466b52ce6d
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-04-22 14:41:17 +0200

    Add date and time functions

---
+++ Gym/MathematicalFunctions/03-date-time-functions.sql
@@ -0,0 +1,28 @@
+-- SELECT EXTRACT(MONTH FROM last_checkin), last_checkin
+-- FROM memberships;
+
+-- SELECT EXTRACT(DAY FROM last_checkin), last_checkin
+-- FROM memberships;
+
+-- SELECT EXTRACT(CENTURY FROM last_checkin), last_checkin
+-- FROM memberships;
+
+-- SELECT EXTRACT(MINUTE FROM last_checkin), last_checkin
+-- FROM memberships;
+
+--DOW postgreSQL only
+-- SELECT EXTRACT(ISODOW FROM last_checkin), last_checkin
+-- FROM memberships; -- Last day in week postgreSQL (DOW - 0 sunday - 7 saturday; ISODOW - 1 monday - 7 sunday)
+
+-- postgreSQL
+SELECT last_checkin::TIMESTAMP::DATE, last_checkin::TIMESTAMP::TIME
+FROM memberships;
+
+-- WEEKDAY mySQL
+-- SELECT WEEKDAY(last_checkin) + 1, last_checkin
+-- FROM memberships;
+
+-- MySQL
+-- SELECT CONVERT(last_checkin, DATE), CONVERT(last_checkin, TIME)
+-- FROM memberships;
+
