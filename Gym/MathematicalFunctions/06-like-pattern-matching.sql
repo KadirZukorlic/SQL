4b7e2773fdf15966944f7efae794d3183204d598
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-04-23 19:25:34 +0200

    LIKE pattern

---
+++ Gym/MathematicalFunctions/06-like-pattern-matching.sql
@@ -0,0 +1,12 @@
+-- SELECT first_name LIKE 'Ma%', first_name
+-- FROM memberships; -- % after Ma selected everything that includes Ma
+
+-- SELECT first_name LIKE '%u%', first_name
+-- FROM memberships;
+
+-- SELECT first_name
+-- FROM memberships
+-- WHERE first_name LIKE 'J____';
+
+-- SELECT first_name ILIKE 'Ma%', first_name
+-- FROM memberships; -- LIKE is case sensitive, ILIKE is NOT case sensitive
