a275dbec7f1c4bb6f76c98df667a6b10643611a1
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-04-24 11:39:56 +0200

    conditional query weekdays by number

--- Gym/MathematicalFunctions/10-conditional-expressions.sql
+++ Gym/MathematicalFunctions/10-conditional-expressions.sql
@@ -4,3 +4,19 @@
 --         ELSE 'Bad Day'
 --     END
 -- FROM orders;
+
+-- MY SQL
+-- SELECT weekday_nr,
+--     CASE WHEN weekday_nr = 1 THEN 'Monday'
+--         WHEN weekday_nr = 2 THEN 'Tuesday'
+--         WHEN weekday_nr = 3 THEN 'Wednesday'
+--         WHEN weekday_nr = 4 THEN 'Thursday'
+--         WHEN weekday_nr = 5 THEN 'Friday'
+--         WHEN weekday_nr = 6 THEN 'Saturday'
+--     ELSE 'Sunday'
+--   END
+-- FROM (
+--     SELECT WEEKDAY(last_checkin) + 1 AS weekday_nr
+--     FROM memberships
+-- ) AS weekday_numbers;
+
