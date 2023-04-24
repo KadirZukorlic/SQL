c7a29306e46d9bdce3fee5061a0bf25e5038e0e0
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-04-24 11:25:35 +0200

    Clean code

--- Gym/MathematicalFunctions/08-subquery-expressions.sql
+++ Gym/MathematicalFunctions/08-subquery-expressions.sql
@@ -3,14 +3,23 @@
 -- WHERE email = 'max@test.com';
 
 
-SELECT EXISTS(
-    SELECT first_name, last_name
-    FROM customers
-    WHERE email = 'max@test.com'
-); -- TRUE
+-- SELECT EXISTS(
+--     SELECT first_name, last_name
+--     FROM customers
+--     WHERE email = 'max@test.com'
+-- ); -- TRUE
 
 -- SELECT EXISTS(
 --     SELECT first_name, last_name
 --     FROM customers
 --     WHERE email = 'xzx@test.com'
 -- ); -- FALSE
+
+-- SELECT o.id
+-- FROM orders AS o
+-- WHERE EXISTS(
+--     SELECT email
+--     FROM customers as c
+--     WHERE o.customer_id = c.id AND c.email = 'manu@test.com'
+-- );
+
