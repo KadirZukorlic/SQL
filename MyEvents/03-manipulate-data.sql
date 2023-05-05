d019686e1a3e9587b3a135b7602566c8440f04c4
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-05-05 12:44:30 +0200

    Insert data

---
+++ MyEvents/03-manipulate-data.sql
@@ -0,0 +1,21 @@
+-- INSERT INTO events (
+--     name,
+--     date_planned,
+--     description,
+--     max_participants,
+--     min_age
+--   )
+-- VALUES (
+--     'GO - Spp',
+--     '2023-05-04 17:00:00',
+--     'Sastanak glavnog odbora sa 6 tacaka dnevnog reda...',
+--     60,
+--     18
+-- ),
+-- (
+--     'OO Tutin SPP',
+--     '2023-08-04 20:00:00',
+--     'Sastanak Opstinskog odbora sa 6 tacaka dnevnog reda...',
+--     20,
+--     16
+-- );
