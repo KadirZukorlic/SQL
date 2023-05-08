dda3d02291dd18e490a12a5179db4f96cf4932b1
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-05-08 17:01:06 +0200

    Aggregate funcs

--- MyEvents/07-queries-with-aggregate-funcs.sql
+++ MyEvents/07-queries-with-aggregate-funcs.sql
@@ -1,2 +1,7 @@
 -- SELECT * FROM users
 -- WHERE first_name LIKE '%Ma%';
+
+-- SELECT COUNT(id) FROM locations;
+-- SELECT SUM(id) FROM locations;
+-- SELECT MAX(id) FROM locations;
+-- SELECT MIN(id) FROM locations;
