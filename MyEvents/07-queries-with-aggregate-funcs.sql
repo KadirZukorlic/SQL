066c790e82bb7bf6879426f77cc49eaf26692234
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-05-08 17:25:44 +0200

    Use HAVING after GROUP BY

--- MyEvents/07-queries-with-aggregate-funcs.sql
+++ MyEvents/07-queries-with-aggregate-funcs.sql
@@ -6,6 +6,11 @@
 -- SELECT MAX(id) FROM locations;
 -- SELECT MIN(id) FROM locations;
 
-SELECT c.name, loc.street, COUNT(loc.id) AS num_locations FROM cities AS c
+-- SELECT c.name, loc.street, COUNT(loc.id) AS num_locations FROM cities AS c
+-- LEFT JOIN locations AS loc ON loc.city_name = c.name
+-- GROUP BY c.name, loc.street;
+
+SELECT c.name, COUNT(loc.id) AS num_locations FROM cities AS c
 LEFT JOIN locations AS loc ON loc.city_name = c.name
-GROUP BY c.name, loc.street;
+GROUP BY c.name
+HAVING COUNT(loc.id) > 1;
